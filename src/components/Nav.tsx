e16444831fab7e2b62197b1976cac7ea3d6ab93b
Raghav Misra <raghav.m2014@gmail.com>
2025-04-12 14:42:13 -0400

    ui stuff

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,20 +2,24 @@ import styled from "styled-components";
 
 const NavContainer = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  
+  .logo {
+    font-size: 1.125rem;
+  }
+
+  .logout {
+    text-align: right;
+    color: var(--burnt-sienna);
+  }
 `;
 
 export function Nav() {
   return (
     <NavContainer className="box lifted">
-      <span className="material-symbols-rounded">menu</span>
-      <div className="logo" style={{ textAlign: "center" }}>
-        BudgetBuddy
-      </div>
-      <span
-        style={{ textAlign: "right" }}
-        className="material-symbols-rounded"
-      >
+      <div className="logo">BudgetBuddy</div>
+      <span className="logout material-symbols-rounded">
         logout
       </span>
     </NavContainer>
